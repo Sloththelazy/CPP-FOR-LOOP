779be2cac91f0813d1b6cd36c6ae4f7420e571e1
Jeevant <jeevantprakharsingh2004@gmail.com>
2022-11-09 01:16:16 +0530

    New commit lecture 7 problem 6 important

---
+++ pattern6_lec7_imp.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout << "Enter the length and width of the pattern";
+    cin >> n;
+    for (int i=1;i<=n;i++)
+    {
+        for (int j =i; j<=n;j++)
+        {
+            cout <<j;
+        }
+        for ( int h =1 ; h<=(i-1);h++)
+        {
+            cout<< h;
+        }
+        cout <<endl;
+    }
+}
